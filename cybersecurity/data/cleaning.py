cc0503393c3c0640a79852b822fc1fde742510b8
David Hay <misterhay@gmail.com>
2023-12-13 14:57:11 -0700

    Move code back to notebook

--- cybersecurity/data/cleaning.py
+++ cybersecurity/data/cleaning.py
@@ -1,25 +1,3 @@
-def start(key):
-    """
-    Sets up the labyrinth API.
-    """
-    import requests
-    import pandas as pd
-    import gzip
-    import io
-    import json
-    import plotly.express as px
-    from IPython import get_ipython
-    def check(query):
-        try:
-            r = requests.get('https://script.google.com/macros/s/AKfycbyNwWgQZCm7MR0HmNAat5nqnbWxc8YviiwZfiVXW4NNQLdPRjVHgLP3DSVKUmyvY3OE/exec?room=Cybersecurity&query='+str(query))
-            response = r.json()
-            get_ipython().run_cell_magic('markdown', '', response['markdown'])
-            get_ipython().set_next_input(response['code'])
-        except:
-            print('Make sure you have entered your answer correctly.')
-            get_ipython().set_next_input("check('')")
-    check(key)
-
 def cleaning(df):
     """
     Cleans the data.
